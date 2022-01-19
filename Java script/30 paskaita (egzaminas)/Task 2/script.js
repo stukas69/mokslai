8557a02d929770b72cde0e63f4bdc865cd5fbffd
stukas <stukas69@gmail.com>
2022-01-19 18:32:14 +0200

    main

---
+++ Java script/30 paskaita (egzaminas)/Task 2/script.js
@@ -0,0 +1,10 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+
+const handleIncrement = () => {
+  count++;
+  totalCount.innerHTML = count;
+};
